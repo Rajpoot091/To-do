e3d4f8b26419684c6c3acd6c0345158a7711c4e8
Muhammad Asad <asadrajpoot0304@gmail.com>
2022-02-03 03:09:19 +0500

    File changes

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "./commponents/Header";
+// import Headers from "./commponents/Header";
 
 export default function App() {
+  const [todos, newTodos] = useState([
+    { name: "work", key: "1" },
+    { name: "Browse", key: "2," },
+    { name: "jim", key: "3" },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header text="Hello Asad" />
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+          />
+
+          <StatusBar style="auto" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +31,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 30,
+  },
+  list: {
+    marginTop: 20,
+    fontWeight: "bold",
   },
 });
---
+++ commponents/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default Header = ({ text }) => {
+  const styles = StyleSheet.create({
+    header: {
+      height: 80,
+      paddingTop: 38,
+      backgroundColor: "coral",
+    },
+
+    list: {
+      textAlign: "center",
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+  });
+
+  console.log(text);
+  return (
+    <View style={styles.header}>
+      <Text style={styles.list}>Todo List</Text>
+    </View>
+  );
+};
